241c06c8097a332ea47551ede42c486a79cbc7bc
ruchika610 <ruchika6101994@gmail.com>
2022-04-28 10:30:57 +0530

    Time: 2 ms (26.67%), Space: 47.3 MB (6.20%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        
+        boolean check=false; int temp=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i%10 == nums[i])
+            {
+                check =true;
+                temp=i;
+                break;
+            }
+        }
+        if(check)
+        {
+            return temp;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
