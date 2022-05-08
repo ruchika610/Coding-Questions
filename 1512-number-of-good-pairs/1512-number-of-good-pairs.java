93264b69144efdc9cfd18700c21691a90c49ff3e
ruchika610 <ruchika6101994@gmail.com>
2022-05-08 13:59:27 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (27.38%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,6 +1,17 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-       int cnt=0,ans=0;
+        int count = 0;
+        int [] freq = new int[101];
+        if(nums.length == 0){
+            return 0;
+        }
+
+        for(int i = 0; i< nums.length; i++){
+             count += freq[nums[i]]++;
+         }
+          return count;
+      /* brute force approach
+      int cnt=0,ans=0;
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
@@ -8,6 +19,7 @@ class Solution {
                 }
             }
         }
-         return cnt;      
+         return cnt;   
+         */
     }
 }
